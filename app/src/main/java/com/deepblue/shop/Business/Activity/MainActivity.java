58909c334ba1ca92dc1a1aad25108958e378dd3e
bruceyang <yang>
2016-05-20 16:18:45 +0800

    初始化，搭建基本框架

---
+++ app/src/main/java/com/deepblue/shop/Business/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.deepblue.shop.Business.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.deepblue.shop.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
