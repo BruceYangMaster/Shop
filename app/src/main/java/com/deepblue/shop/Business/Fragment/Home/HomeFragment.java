8aa5218be87ff419b2f909bf55dc064d0b6789e7
bruceyang <yang>
2016-05-23 15:53:03 +0800

    修改了home

--- app/src/main/java/com/deepblue/shop/Business/Fragment/Home/HomeFragment.java
+++ app/src/main/java/com/deepblue/shop/Business/Fragment/Home/HomeFragment.java
@@ -8,16 +8,28 @@ import android.view.ViewGroup;
 
 import com.deepblue.shop.R;
 
+/**
+ * 主页的fragment，该布局要求非常灵活，所以比较复杂
+ */
 public class HomeFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
 
-
+    /**
+     * 默认的一个无参数的构造函数
+     */
     public HomeFragment() {
     }
 
+    /**
+     * newInstance
+     *
+     * @param param1
+     * @param param2
+     * @return
+     */
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
@@ -27,6 +39,11 @@ public class HomeFragment extends Fragment {
         return fragment;
     }
 
+    /**
+     * 初始化
+     *
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +53,18 @@ public class HomeFragment extends Fragment {
         }
     }
 
+    /**
+     * 创建布局
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
-
 }
